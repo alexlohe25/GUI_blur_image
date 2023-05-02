60a22c48a2901a462f09a67d02c22a3a761c1f86
Fernando Jimenez Pereyra <a01734609@tec.mx>
2023-05-02 14:51:14 -0600

    Comprobaciones

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import *
 
 import sys
 import os
+import puremagic
 
 #////////////////////////////////////////////////
 
@@ -13,6 +14,7 @@ class MainWidget(QMainWindow):
         self.setWindowTitle("Blur it")
         self.resize(720, 480)
         self.setAcceptDrops(True)
+        self.kernel = 0
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
@@ -24,14 +26,47 @@ class MainWidget(QMainWindow):
     def dropEvent(self, event):
         files = [u.toLocalFile() for u in event.mimeData().urls()]
         for f in files:
-            blurItMessage(f, " va a ser blureado")
-            os.system("echo {} && sleep 5".format(f))
-            blurItMessage(f, " ha sido blureado")
+            isBMP = checkIfIsBMP(f)
+            if isBMP:
+                print(isBMP)
+                if blurItMessage(f):
+                    print(isBMP)
+                    self.setKernel()
+                    print("{} sera blureado en {} mascaras".format(f, self.kernel))
+                    os.system("mpiexec -n {} -host 127.0.0.1 ./mpi_hello".format(f))
+                    blurredMessage(f)
 
-def blurItMessage(file, message):
+    def setKernel(self):
+        i, okPressed = QInputDialog.getInt(self, "Set kernel mask","Amount:", 1, 1, 50, 1)
+        if okPressed:
+            self.kernel = i
+
+def blurItMessage(file):
     alert = QMessageBox()
-    alert.setText(file + message)
+    alert.setText(file + " va a ser blureado")
+    alert.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+
+    return_value = alert.exec()
+    if return_value == QMessageBox.Ok:
+        return True
+    
+    else:
+        return False
+
+def blurredMessage(file):
+    alert = QMessageBox()
+    alert.setText(file + " ha sido blureado")
     alert.exec_()
+
+def checkIfIsBMP(file):
+    format = puremagic.from_file(file)
+    if format == ".bmp":
+        return True
+    else:
+        alert = QMessageBox()
+        alert.setText(file + " no es una imagen en formato BMP")
+        alert.exec_()
+        return False
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
